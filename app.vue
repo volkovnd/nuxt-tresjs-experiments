e3f83011bf0f0eda57fef799e14aa6b967bbbd8f
Volkov Nikolay <volkovnd@yandex.ru>
2025-07-10 23:34:04 +0300

    feat: Added initial tresjs scene

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <TresCanvas
+    clear-color="#82DBC5"
+    window-size
+  >
+    <TresPerspectiveCamera
+      :position="[3, 3, 3]"
+      :look-at="[0, 0, 0]"
+    />
+    <TresMesh>
+      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+      <TresMeshBasicMaterial color="orange" />
+    </TresMesh>
+
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@tresjs/nuxt'],
   devtools: { enabled: true, telemetry: false },
   compatibilityDate: '2025-05-15',
   eslint: {
     config: {
       stylistic: true,
     },
+  },
+  tres: {
+    devtools: true,
   },
 })
