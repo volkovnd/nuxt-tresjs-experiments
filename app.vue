6bdada45663fddc40fd2c89e5c99ab227f398f16
Volkov Nikolay <volkovnd@yandex.ru>
2025-07-11 03:21:37 +0300

    feat: added some 3d elements

--- app.vue
+++ app.vue
@@ -1,17 +1,62 @@
 <template>
   <TresCanvas
-    clear-color="#82DBC5"
     window-size
+    shadows
+    clear-color="#B8B8B8"
   >
     <TresPerspectiveCamera
-      :position="[3, 3, 3]"
+      :position="[0, 0, 8]"
       :look-at="[0, 0, 0]"
     />
-    <TresMesh>
-      <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
-      <TresMeshBasicMaterial color="orange" />
-    </TresMesh>
 
-    <TresAmbientLight :intensity="1" />
+    <VCircle
+      :position="[-2.5, 1.25, 0]"
+      :rotation="[0, -Math.PI / 16, 0]"
+      color="blue"
+    />
+
+    <VCircle
+      :position="[-1.25, 0, 0]"
+      :rotation="[0, Math.PI / 16, 0]"
+      color="yellow"
+    />
+
+    <VCircle
+      :position="[0, 1.25, 0]"
+      :rotation="[0, -Math.PI / 16, 0]"
+      color="black"
+    />
+
+    <VCircle
+      :position="[1.25, 0, 0]"
+      :rotation="[0, Math.PI / 16, 0]"
+      color="green"
+    />
+
+    <VCircle
+      :position="[2.5, 1.25, 0]"
+      :rotation="[0, -Math.PI / 16, 0]"
+      color="red"
+    />
+
+    <TresDirectionalLight
+      cast-shadow
+      :position="[0, 2, 5]"
+      :intensity="1"
+    />
   </TresCanvas>
 </template>
+
+<script setup lang="ts">
+useHead({
+  htmlAttrs: {
+    lang: 'ru'
+  },
+  link: [
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.ico'
+    }
+  ]
+})
+</script>
---
+++ components/VCircle.client.vue
@@ -0,0 +1,24 @@
+<template>
+  <TresMesh
+    cast-shadow
+    :position="position"
+    :rotation="rotation"
+  >
+    <TresTorusGeometry
+      :args="[1.1, 0.15, 16, 32]"
+    />
+    <TresMeshToonMaterial
+      :color="color"
+    />
+  </TresMesh>
+</template>
+
+<script lang="ts" setup>
+withDefaults(defineProps<{
+  color: string
+  position: [number, number, number]
+  rotation?: [number, number, number]
+}>(), {
+  rotation: () => [0, 0, 0],
+})
+</script>
