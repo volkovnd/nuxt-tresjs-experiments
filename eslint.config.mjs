b2e6a1cb042fa80dcf54dad03be6d6d4894b9d86
Volkov Nikolay <volkovnd@yandex.ru>
2025-07-11 03:21:24 +0300

    chore: improved eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,34 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt([
+  {
+    rules: {
+      '@stylistic/object-curly-newline': [
+        'error',
+        'always'
+      ],
+      '@stylistic/object-property-newline': [
+        'error',
+        {
+          allowAllPropertiesOnSameLine: false
+        }
+      ],
+      '@stylistic/array-element-newline': [
+        'error',
+        {
+          minItems: 2,
+          consistent: false,
+          multiline: true
+        }
+      ],
+      '@stylistic/array-bracket-newline': [
+        'error',
+        {
+          minItems: 2,
+          multiline: true
+        }
+      ]
+    }
+  }
+])
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@tresjs/nuxt'],
-  devtools: { enabled: true, telemetry: false },
+  modules: [
+    '@nuxt/eslint',
+    '@tresjs/nuxt'
+  ],
+  devtools: {
+    enabled: true,
+    telemetry: false
+  },
   compatibilityDate: '2025-05-15',
+  telemetry: {
+    enabled: false
+  },
   eslint: {
     config: {
-      stylistic: true,
-    },
+      nuxt: {
+        sortConfigKeys: true
+      },
+      stylistic: {
+        semi: false,
+        arrowParens: true,
+        blockSpacing: true,
+        commaDangle: 'never',
+        indent: 2,
+        quotes: 'single'
+      }
+    }
   },
   tres: {
-    devtools: true,
-  },
+    devtools: true
+  }
 })
